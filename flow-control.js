0e2f213516bcb3970079b316420259c6a6abfc12
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-09 18:00:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,16 +7,14 @@ function basicTeenager(age) {
 function teenager(age){
       if (age >= 13 && age <= 19) {
       return "You are a teenager!";
-} else {
-      return "You are not a teenager";
+} else {return "You are not a teenager";
 }
 }
 
 function ageChecker(age){
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  } else if (age a<=12) {
-        return "You are a kid";
+  } else if (age a<=12) {return "You are a kid";
   }
   else {return "You are a grownup";}
 }
@@ -28,5 +26,12 @@ function ternaryTeenager(age){
 
 function switchAge(age){
   switch (age) {
+case "13":console.log("You are a teenager!")
+case "14":console.log("You are a teenager!")
+case "15":console.log("You are a teenager!")
+case "16":console.log("You are a teenager!")
+case "17":console.log("You are a teenager!")
+case "18":console.log("You are a teenager!")
+case "19":console.log("You are a teenager!")
   }
 }
